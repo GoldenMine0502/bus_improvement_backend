383346884f8344c7208d683a71b10bdbde87b8c7
GoldenMine0502 <ehe123@naver.com>
2022-11-09 20:02:27 +0900

    다익스트라 알고리즘

--- src/main/kotlin/kr/goldenmine/bus_improvement_backend/calc/BusCalculator.kt
+++ src/main/kotlin/kr/goldenmine/bus_improvement_backend/calc/BusCalculator.kt
@@ -1,13 +1,13 @@
 package kr.goldenmine.bus_improvement_backend.calc
 
-import kr.goldenmine.bus_improvement_backend.BusController
-import kr.goldenmine.bus_improvement_backend.models.station.BusStopStationInfo
 import kr.goldenmine.bus_improvement_backend.models.station.BusStopStationSerivce
 import kr.goldenmine.bus_improvement_backend.models.through.BusThroughInfoSerivce
 import kr.goldenmine.bus_improvement_backend.models.traffic.BusTrafficSerivce
+import kr.goldenmine.bus_improvement_backend.util.distance
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class BusCalculator(
@@ -53,21 +53,79 @@ class BusCalculator(
 //        }
 
         // 노드 생성
-        val nodes = ArrayList<ArrayList<Int>>()
+        val nodes = ArrayList<ArrayList<Node>>()
         repeat(stations.size + 1) {
             nodes.add(ArrayList())
         }
 
         for(i in 0 until throughs.size - 1) {
+            val startThrough = throughs[i]
+            val finishThrough = throughs[i + 1]
             val start = stationsMap[throughs[i].busStopStationId]
             val finish = stationsMap[throughs[i + 1].busStopStationId]
 
+
             if(start != null && finish != null) {
-                nodes[start].add(finish)
+                val stationStart = stations[start]
+                val stationFinish = stations[finish]
+
+                if(stationStart.posX != null && stationStart.posY != null
+                    && stationFinish.posX != null && stationFinish.posY != null) {
+                    val distance = distance(
+                        stationStart.posX, stationFinish.posX,
+                        stationStart.posY, stationFinish.posY,
+                        0.0, 0.0
+                    )
+
+                    nodes[start].add(Node(finish, distance))
+                }
             }
         }
 
         // 다익스트라 알고리즘 실행
+        val dist = DoubleArray(stations.size )
+        val previous = IntArray(stations.size)
 
+        for (i in dist.indices) {
+            dist[i] = Double.MAX_VALUE
+            previous[i] = i
+        }
+
+        val start = 0
+
+        val queue = PriorityQueue<Node>(Comparator.comparingDouble { o -> o.cost })
+        queue.offer(Node(start, 0.0))
+        dist[start] = 0.0
+
+        while (!queue.isEmpty()) {
+            val current = queue.poll()
+
+            if (dist[current.index] < current.cost)
+                continue
+
+            val nexts = nodes[current.index]
+
+            for (i in nexts.indices) {
+                val next: Node = nexts[i]
+                val nextCost = current.cost + next.cost
+                if (dist[next.index] > nextCost) {
+                    dist[next.index] = nextCost
+                    previous[next.index] = current.index
+                    queue.add(Node(next.index, dist[next.index]))
+                }
+            }
+        }
+    }
+
+    // 다음 노드의 인덱스와, 그 노드로 가는데 필요한 비용을 담고 있다.
+    inner class Node(
+        val index: Int, val cost: Double
+    ) {
+        override fun toString(): String {
+            return "Node{" +
+                    "idx=" + index +
+                    ", cost=" + cost +
+                    '}'
+        }
     }
 }
