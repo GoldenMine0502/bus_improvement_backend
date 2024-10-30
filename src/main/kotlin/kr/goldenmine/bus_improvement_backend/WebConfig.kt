1f95708f2f0bb751d207f235104998d0d4f98646
GoldenMine0502 <ehe123@naver.com>
2024-10-30 10:53:03 +0900

    음

--- src/main/kotlin/kr/goldenmine/bus_improvement_backend/WebConfig.kt
+++ src/main/kotlin/kr/goldenmine/bus_improvement_backend/WebConfig.kt
@@ -8,7 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+//        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+//            .allowedOrigins("http://home.goldenmine.kr:3000", "http://locahost:3000") // 특정 출처만 허용
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .allowedHeaders("*")
+//            .allowCredentials(true); // 인증 포함 허용
     }
 }
